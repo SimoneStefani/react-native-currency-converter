7964cd5ae01d61f20b33a6f2535878aafde13c38
Simone Stefani <s.stefani95@gmail.com>
2018-02-01 23:30:37 +0100

    Add reducers for currencies

--- app/reducers/currencies.js
+++ app/reducers/currencies.js
@@ -1,4 +1,9 @@
-import { CHANGE_CURRENCY_AMOUNT, SWAP_CURRENCY } from '../actions/currencies';
+import {
+  CHANGE_CURRENCY_AMOUNT,
+  SWAP_CURRENCY,
+  CHANGE_BASE_CURRENCY,
+  CHANGE_QUOTE_CURRENCY,
+} from '../actions/currencies';
 
 // const initialState = {
 //   baseCurrency: 'USD',
@@ -53,6 +58,20 @@ const initialState = {
   },
 };
 
+const setConversions = (state, action) => {
+  let conversion = {
+    isFetching: true,
+    date: '',
+    rate: {},
+  };
+
+  if (state.conversions[action.currency]) {
+    conversion = state.conversions[action.currency];
+  }
+
+  return { ...state.conversions, [action.currency]: conversion };
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case CHANGE_CURRENCY_AMOUNT:
@@ -65,6 +84,18 @@ const reducer = (state = initialState, action) => {
         ...state,
         baseCurrency: state.quoteCurrency,
         quoteCurrency: state.baseCurrency,
+      };
+    case CHANGE_BASE_CURRENCY:
+      return {
+        ...state,
+        baseCurrency: action.currency,
+        conversions: setConversions(state, action),
+      };
+    case CHANGE_QUOTE_CURRENCY:
+      return {
+        ...state,
+        quoteCurrency: action.currency,
+        conversions: setConversions(state, action),
       };
     default:
       return state;
