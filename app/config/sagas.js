0c5d3681d216e00f19ac36d8372c6516360f3c2c
Simone Stefani <s.stefani95@gmail.com>
2018-02-04 23:28:19 +0100

    redux saga requests

--- app/config/sagas.js
+++ app/config/sagas.js
@@ -1,8 +1,31 @@
-import { takeEvery } from 'redux-saga/effects';
-import { SWAP_CURRENCY, CHANGE_BASE_CURRENCY, GET_INITIAL_CONVERSION } from '../actions/currencies';
+import { takeEvery, select, call, put } from 'redux-saga/effects';
+import {
+  SWAP_CURRENCY,
+  CHANGE_BASE_CURRENCY,
+  GET_INITIAL_CONVERSION,
+  CONVERSION_RESULT,
+  CONVERSION_ERROR,
+} from '../actions/currencies';
+
+const getLatestRates = currency => fetch(`http://api.fixer.io/latest?base=${currency}`);
 
 function* fetchLatestConversionRates(action) {
-  yield;
+  try {
+    let { currency } = action;
+    if (currency === undefined) {
+      currency = yield select(state => state.currencies.baseCurrency);
+    }
+    const response = yield call(getLatestRates, currency);
+    const result = yield response.json();
+
+    if (result.error) {
+      yield put({ type: CONVERSION_ERROR, error: result.error });
+    } else {
+      yield put({ type: CONVERSION_RESULT, result });
+    }
+  } catch (e) {
+    yield put({ type: CONVERSION_ERROR, error: e.message });
+  }
 }
 
 export default function* rootSaga() {
